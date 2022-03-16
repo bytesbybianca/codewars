5bc78fdd1437ac190cbecf4ac504f75b92bf77e0
Bianca Togonon <biancatogodev@gmail.com>
2022-03-15 19:50:06 -0700

    first codewars

---
+++ js/number-to-string.js
@@ -0,0 +1,9 @@
+function numberToString(num) {
+    // Return a string of the number here!
+    return String(num)
+  }
+
+  //Best practice solution
+  // function numberToString(num) {
+  //   return num.toString();
+  // }

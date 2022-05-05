9e08a45a628eb0ab7ba0f2b4c71949c0a863cd47
Bianca Togonon <biancatogodev@gmail.com>
2022-05-05 11:52:37 -0700

    5/5/22 challenge

---
+++ 8kyu/8-grasshopper-variable-assignment.js
@@ -0,0 +1,25 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Grasshopper - Variable Assignment Debug
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/5612e743cab69fec6d000077
+
+
+// INSTRUCTIONS
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+
+// SOLUTION
+var a = "dev"
+var b = "Lab"
+
+var name = `${a}${b}`
+
+// // TEST CASES
+console.log(a, 'dev')
+console.log(b, 'Lab')
+console.log(name, 'devLab')

a379abce9f906b93e45d8554916d8328e306822f
Bianca Togonon <biancatogodev@gmail.com>
2022-05-07 10:11:21 -0700

    5/7/22 challenge

---
+++ 8kyu/8-basic-training-add-to-array.js
@@ -0,0 +1,26 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Basic Training: Add item to an Array
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/511f0fe64ae8683297000001
+
+
+// INSTRUCTIONS
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+
+// SOLUTION
+websites.push('codewars')
+
+
+
+// // TEST CASES
+console.log(websites)

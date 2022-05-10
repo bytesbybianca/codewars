7a3bb26ece109b45260cb82e37680cc37cc58859
Bianca Togonon <biancatogodev@gmail.com>
2022-05-09 22:46:15 -0700

    5/9/22 challenge

---
+++ 6kyu/6-unique-in-order.js
@@ -0,0 +1,29 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Unique In Order
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/54e6533c92449cc251001667
+
+
+// INSTRUCTIONS
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+
+// SOLUTION
+var uniqueInOrder=function(iterable){
+  // find unique characters
+  // let commons = []
+  let arr = []
+  if(typeof iterable == 'string') iterable = iterable.split('')
+
+    iterable.map((num, i) => iterable[i+1] == num ? null : arr.push(num))
+  // return in order
+  return arr
+}
+
+// // TEST CASES
+console.log(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+console.log(uniqueInOrder([1,2,2,3,3]), [1,2,3])

bc3818f0ff92735926861052894e178272afa09b
Bianca Togonon <biancatogodev@gmail.com>
2022-05-11 21:30:31 -0700

    5/11/22 challenge

---
+++ 7kyu/7-pirates-are-the-cannons-ready.js
@@ -0,0 +1,47 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Pirates!! Are the Cannons ready!??
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/5748a883eb737cab000022a6
+
+
+// INSTRUCTIONS
+// Ahoy Matey!
+
+// Welcome to the seven seas.
+
+// You are the captain of a pirate ship.
+
+// You are in battle against the royal navy.
+
+// You have cannons at the ready.... or are they?
+
+// Your task is to check if the gunners are loaded and ready, if they are: Fire!
+
+// If they aren't ready: Shiver me timbers!
+
+// Your gunners for each test case are 2, 3 or 4.
+
+// When you check if they are ready their answers are in a dictionary and will either be: aye or nay
+
+// Firing with less than all gunners ready is non-optimum (this is not fire at will, this is fire by the captain's orders or walk the plank, dirty sea-dog!)
+
+// If all answers are 'aye' then Fire! if one or more are 'nay' then Shiver me timbers!
+
+// SOLUTION
+const cannonsReady = (gunners) => {
+  for(let key in gunners) {
+    if(gunners[key] == 'nay') {
+      return 'Shiver me timbers!'
+    }
+  } return 'Fire!'
+}
+
+// TEST CASES
+let a = {'Mike':'aye','Joe':'aye','Johnson':'aye','Peter':'aye'};
+let b = {'Mike':'aye','Joe':'nay','Johnson':'aye','Peter':'aye'};
+console.log(cannonsReady(a), 'Fire!');
+console.log(cannonsReady(b), 'Shiver me timbers!');

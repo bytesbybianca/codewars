61cc6cf758b4fb1d23266ca34eb342671044a6a3
Bianca Togonon <biancatogodev@gmail.com>
2022-06-08 09:48:41 -0700

    6/8/2022 challenge

---
+++ 8kyu/8-speedcode-2.js
@@ -0,0 +1,40 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// SpeedCode #2 - Array Madness
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1
+
+
+/* INSTRUCTIONS
+
+SpeedCode #2 - Array Madness
+Objective
+Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+E.g.
+
+arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+Get your timer out. Are you ready? Ready, get set, GO!!!
+
+*/
+
+// SOLUTION
+function arrayMadness(a, b) {
+  return a.map(x => x**2).reduce((acc, c) => acc + c, 0) > b.map(x => x**3).reduce((acc, c) => acc + c, 0)
+}
+
+// TEST CASES
+console.log(arrayMadness([4,5,6],[1,2,3]), true);
+console.log(arrayMadness([5,6,7],[4,5,6]), false);
+console.log(arrayMadness([4,5,6],[3,4,5]), false);
+console.log(arrayMadness([3,4,5],[2,3,4]), false);
+console.log(arrayMadness([2,3,4],[1,2,3]), false);
+console.log(arrayMadness([1,2,3],[0,1,2]), true);
+console.log(arrayMadness([5,3,2,4,1],[15]), false);
+console.log(arrayMadness([2,5,3,4,1],[3,3,3,3,3]), false);
+console.log(arrayMadness([1,3,5,2,4],[2,2,2,2,2,2,2,1]), false);
+console.log(arrayMadness([1,2,3,4,5],[2,2,2,2,2,2,1,1,1]), true);
+console.log(arrayMadness([2,4,6,8,10,12,14],[1,3,5,7,9,11,13]), false);

0ea1e3c6317d03f892e13bfd4f5b281260b06b25
Bianca Togonon <biancatogodev@gmail.com>
2022-06-10 21:45:28 -0700

    6/10/2022 challenge

---
+++ 6kyu/6-break-camelCase.js
@@ -0,0 +1,37 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Break camelCase
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/5208f99aee097e6552000148
+
+
+// INSTRUCTIONS
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// PREP
+// P: a string empty or filled with either camel casing or not
+// R: empty strings => empty strings,
+    // camel casing => words separated by space
+    // no camel casing => return as is
+// E:
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+// P:
+
+// SOLUTION
+function solution(string) {
+      // split to each character
+  return string.split('')
+  // check if capital
+  .map((x, i) => x === x.toUpperCase() ? ` ${x}` : x)
+  // if capital, split and index minus 1
+  .join('')
+}
+
+// // TEST CASES
+console.log(solution('camelCasing'), 'camel Casing')
+console.log(solution('camelCasingTest'), 'camel Casing Test')

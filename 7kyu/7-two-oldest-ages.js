67b6b4d7cddb6dc3fe71ecacba7749fe8c1da2d9
Bianca Togonon <biancatogodev@gmail.com>
2022-06-24 18:08:18 -0700

    6/24/22 challenge

---
+++ 7kyu/7-two-oldest-ages.js
@@ -0,0 +1,34 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Two Oldest Ages
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/511f11d355fe575d2c000001
+
+
+/* INSTRUCTIONS
+
+The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+For example (Input --> Output):
+
+[1, 2, 10, 8] --> [8, 10]
+[1, 5, 87, 45, 8, 8] --> [45, 87]
+[1, 3, 10, 0]) --> [3, 10]
+
+*/
+
+// SOLUTION
+function twoOldestAges(ages){
+    let sorted = ages.sort((a,b) => a - b)
+    return [sorted[sorted.length -2], sorted[sorted.length -1]]
+}
+
+
+// TEST CASES
+console.log(twoOldestAges([1,5,87,45,8,8]), [45, 87]);
+console.log(twoOldestAges([6,5,83,5,3,18]), [18, 83]);

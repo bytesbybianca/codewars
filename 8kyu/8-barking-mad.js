b1c4153b0f7b6eda207efbb698743a2c8a7ba732
Bianca Togonon <biancatogodev@gmail.com>
2022-09-03 17:18:43 -0700

    9/3/2022 challenge

---
+++ 8kyu/8-barking-mad.js
@@ -0,0 +1,41 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+/*/////////////////////////////////////////
+Barking mad
+*//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+/* URL
+https://www.codewars.com/kata/54dba07f03e88a4cec000caf
+*/
+
+/* INSTRUCTIONS
+
+Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined
+Use method prototypes to enable all Dogs to bark.
+
+*/
+
+// SOLUTION
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+scoobydoo.bark = function() {
+  return "Woof";
+};
+
+
+// TEST CASES
+console.log(snoopy.bark() === "Woof")
+console.log(scoobydoo.bark() === "Woof")

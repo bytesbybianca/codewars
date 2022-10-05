908bf9b84b3b65fce966ddbf6c0efb80dc5a6434
Bianca Togonon <biancatogodev@gmail.com>
2022-10-05 12:17:58 -0700

    10/5/2022 challenge

---
+++ 8kyu/8-simple-comparison.js
@@ -0,0 +1,30 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+/*/////////////////////////////////////////
+Simple Comparison?
+*//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+/* URL
+https://www.codewars.com/kata/57f6ecdfcca6e045d2001207/
+*/
+
+/* INSTRUCTIONS
+
+Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+
+*/
+
+// SOLUTION
+function add(a, b){
+	return a == b
+}
+
+// TEST CASES
+console.log(add('1', 1), true);
+console.log(add(1, '1'), true);
+console.log(add(1, '0'), false);
+console.log(add('11', 11), true);
+console.log(add(12, 12), true);
+console.log(add(120, '021'), false);

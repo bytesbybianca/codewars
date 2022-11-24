e26e46182291839be5b125ed608528fb674d6ca4
Bianca Togonon <biancatogodev@gmail.com>
2022-11-23 20:00:15 -0800

    11/23 challenge

---
+++ 8kyu/8-return-to-sanity.js
@@ -0,0 +1,27 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+/*/////////////////////////////////////////
+Return to Sanity
+*//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+/* URL
+https://www.codewars.com/kata/514a7ac1a33775cbb500001e/
+*/
+
+/* INSTRUCTIONS
+
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+
+
+*/
+
+// SOLUTION
+function mystery() {
+  var results = {sanity: 'Hello'};
+  return results;
+}
+
+
+// TEST CASES
+console.log(mystery(), {sanity: 'Hello'}, 'Mystery has not returned to sanity');

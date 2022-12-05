935a624f5d8ed708cad1e311dd1cf97a46bef978
Bianca Togonon <biancatogodev@gmail.com>
2022-12-04 22:28:42 -0800

    12/4 challenge

---
+++ 7kyu/7-palindrome-chain-len.js
@@ -0,0 +1,63 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+/*/////////////////////////////////////////
+Palindrome chain length
+*//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+/* URL
+https://www.codewars.com/kata/525f039017c7cd0e1a000a26
+*/
+
+/* INSTRUCTIONS
+
+Number is a palindrome if it is equal to the number with digits in reversed order. For example, 5, 44, 171, 4884 are palindromes, and 43, 194, 4773 are not.
+
+Write a function which takes a positive integer and returns the number of special steps needed to obtain a palindrome. The special step is: "reverse the digits, and add to the original number". If the resulting number is not a palindrome, repeat the procedure with the sum until the resulting number is a palindrome.
+
+If the input number is already a palindrome, the number of steps is 0.
+
+All inputs are guaranteed to have a final palindrome which does not overflow MAX_SAFE_INTEGER.
+
+Example
+For example, start with 87:
+
+  87 +   78 =  165     - step 1, not a palindrome
+ 165 +  561 =  726     - step 2, not a palindrome
+ 726 +  627 = 1353     - step 3, not a palindrome
+1353 + 3531 = 4884     - step 4, palindrome!
+4884 is a palindrome and we needed 4 steps to obtain it, so answer for 87 is 4.
+
+Additional info
+Some interesting information on the problem can be found in this Wikipedia article on Lychrel numbers.
+
+*/
+
+// SOLUTION
+var palindromeChainLength = function(n) {
+  let reversedNum = Number(n.toString().split('').reverse().join(''))
+  if(n === reversedNum) return 0
+  let count = 0
+
+  while(n !== reversedNum) {
+    n += reversedNum
+    reversedNum = Number(n.toString().split('').reverse().join(''))
+    count++
+  }
+  // let count = 0
+  // let palindrome = Number((n + '').split('').reverse().join(''));
+  // while (palindrome !== n) {
+  //   n = Number(n) + Number(palindrome);
+  //   palindrome = Number((n + '').split('').reverse().join(''));
+  //   count++
+  // }
+
+  return count
+};
+
+
+// TEST CASES         
+console.log(palindromeChainLength(1),  0);
+console.log(palindromeChainLength(88), 0);    
+console.log(palindromeChainLength(87), 4);
+console.log(palindromeChainLength(89), 24);
+console.log(palindromeChainLength(10), 1);

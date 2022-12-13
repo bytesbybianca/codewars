3ec8bcc41f1769b9dbba2d9786c3ccc5593ed935
Bianca Togonon <biancatogodev@gmail.com>
2022-12-12 21:07:52 -0800

    12/12 challenge

---
+++ 8kyu/8-chuck-norris-vii.js
@@ -0,0 +1,29 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+/*/////////////////////////////////////////
+Chuck Norris VII - True or False? (Beginner)
+*//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+/* URL
+https://www.codewars.com/kata/570669d8cb7293a2d1001473
+*/
+
+/* INSTRUCTIONS
+
+It's a well known fact that anything Chuck Norris wants, he gets. As a result Chuck very rarely has to use the word false.
+
+It is a less well known fact that if something is true, and Chuck doesn't want it to be, Chuck can scare the truth with his massive biceps, and it automatically becomes false.
+
+Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+
+Go show some truth who's boss!
+
+*/
+
+// SOLUTION
+function ifChuckSaysSo(){
+  return 'Chuck' === 'chuck'
+}
+
+// TEST CASES         
+console.log(ifChuckSaysSo(), false);

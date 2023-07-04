4f98d733075dc98069cb0daa4446ad3eb6b3ac5f
Bianca Togonon <biancatogodev@gmail.com>
2023-07-04 11:59:50 -0700

    7/4 challenge

---
+++ 7kyu/7-reverse-list.js
@@ -0,0 +1,26 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+/*/////////////////////////////////////////
+7 kyu
+Reverse list
+*/ /////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+/* URL
+https://www.codewars.com/kata/57a04da9e298a7ee43000111
+*/
+
+/* INSTRUCTIONS
+
+Write reverseList function that simply reverses lists.
+
+*/
+/* SOLUTION
+
+*/
+function reverseList(arr) {
+	return arr.reverse();
+}
+
+// TEST CASES
+console.log(reverseList([]), []);
+console.log(reverseList([1, 2, 3]), [3, 2, 1]);

42774e18a8f2e1d0a2b8c2f8fcd77d81299a8d39
Bianca Togonon <biancatogodev@gmail.com>
2024-01-26 17:45:16 -0800

    1/26 challenge

---
+++ 8kyu/8-are-they-opp.js
@@ -0,0 +1,48 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+/*/////////////////////////////////////////
+8 kyu
+They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?
+*/ /////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+/* URL
+https://www.codewars.com/kata/574b1916a3ebd6e4fa0012e7
+*/
+
+/* INSTRUCTIONS
+
+Task
+Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result should be a boolean value, instead of a string.
+
+The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are empty then you should return false/False.
+
+Examples (input -> output)
+"ab","AB"     -> true
+"aB","Ab"     -> true
+"aBcd","AbCD" -> true
+"AB","Ab"     -> false
+"",""         -> false
+
+*/
+/* SOLUTION
+
+*/
+function isOpposite(s1, s2) {
+	if (!s1 && !s2) return false;
+	for (let i = 0; i < s1.length; i++) {
+		if (s1[i] === s1[i].toLowerCase()) {
+			if (s1[i].toUpperCase() !== s2[i]) return false;
+		} else {
+			if (s1[i].toLowerCase() !== s2[i]) return false;
+		}
+	}
+	return true;
+}
+
+// TEST CASES
+console.log(isOpposite("ab", "AB"), true);
+console.log(isOpposite("aB", "Ab"), true);
+console.log(isOpposite("aBcd", "AbCD"), true);
+console.log(isOpposite("aBcde", "AbCD"), false);
+console.log(isOpposite("AB", "Ab"), false);
+console.log(isOpposite("", ""), false);
